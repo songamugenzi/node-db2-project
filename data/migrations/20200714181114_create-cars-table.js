446ba63c6b76e32f102b172a75f39458f2a45861
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-07-15 18:34:32 -0400

    Fixed bugs from 1:1

--- data/migrations/20200714181114_create-cars-table.js
+++ data/migrations/20200714181114_create-cars-table.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable("cars", (tbl) => {
     tbl.increments("carID");
-    tbl.string("VIN", 128).notNullable();
+    tbl.string("VIN", 128).notNullable().unique();
     tbl.string("Make", 128).notNullable();
     tbl.string("Model", 128).notNullable();
     tbl.integer("Mileage", 128).notNullable();
