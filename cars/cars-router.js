a410a607e822cf59679262c99ed2a6d8eb9d7b90
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-09-21 17:00:34 -0400

    Met project MVP and created PUT and DELETE endpoints for stretch

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -34,4 +34,37 @@ router.post("/", (req, res) => {
     });
 });
 
+router.put("/:carID", (req, res) => {
+  const { carID } = req.params;
+  const changes = req.body;
+
+  db("cars")
+    .where({ carID })
+    .update(changes)
+    .then((count) => {
+      if (count) {
+        res.status(200).json({ updated: count });
+      } else {
+        res.status(404).json({ message: "Car ID not found" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: "Error updating car", error });
+    });
+});
+
+router.delete("/:carID", async (req, res) => {
+  const { carID } = req.params;
+
+  try {
+    const count = await db("cars").where({ carID }).del();
+    count
+      ? res.status(200).json({ deleted: count })
+      : res.status(404).json({ message: "Car ID not found" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting car", error });
+  }
+});
+
 module.exports = router;
